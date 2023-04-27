dc0c42e7fea7a2678802d3e2f70aed9279dfb7bc
dyussenov <dyusenov.aset.arm@gmail.com>
2023-04-28 03:22:58 +0600

    add forms for admin

--- audit/admin.py
+++ audit/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+from .forms import ItemForm
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    add_form = ItemForm
+    form = ItemForm
+    list_display = ['id', 'name', 'category', 'amortization', 'operation_life', 'receive_date', 'expiration_date']
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category)
--- audit/models.py
+++ audit/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=60)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    price = models.FloatField()
+    amortization = models.IntegerField()
+    operation_life = models.IntegerField()
+    receive_date = models.DateField(auto_now_add=True)
+    expiration_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        self.expiration_date = datetime.datetime.now() + datetime.timedelta(days=self.operation_life * 30)
+
+        # call the save() method of the parent
+        super(Item, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
