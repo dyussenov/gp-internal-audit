d504c03f891d6d5b0c9cf31d9f9e546fb4e96584
dyussenov <dyusenov.aset.arm@gmail.com>
2023-04-28 02:04:03 +0600

    add audit app

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'audit.apps.AuditConfig',
 ]
 
 MIDDLEWARE = [
