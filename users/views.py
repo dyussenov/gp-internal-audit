cc01eebc0555a68a9220e60d618cd942bf48d082
dyussenov <dyusenov.aset.arm@gmail.com>
2023-04-28 05:47:10 +0600

    fix home template

--- users/views.py
+++ users/views.py
@@ -3,6 +3,16 @@ from audit import models as audit_models
 
 
 def home(request):
-    storage = audit_models.Storage.objects.all()
-    context = {'storage': storage}
+    cats = {}
+
+    categories = audit_models.Category.objects.all()
+    for cat in categories:
+        objs = audit_models.Storage.objects.filter(item__category=cat)
+        print(objs)
+        cats[str(cat)] = objs
+
+    context = {
+        'cats': cats
+    }
+    print(context)
     return render(request, 'home.html', context)
