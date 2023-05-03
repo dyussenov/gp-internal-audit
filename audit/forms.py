e4999ddae5c92b5de9179b639fcc31980a9bee25
dyussenov <dyusenov.aset.arm@gmail.com>
2023-05-03 23:44:33 +0600

    fix form fields labels

--- audit/forms.py
+++ audit/forms.py
@@ -6,17 +6,40 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = '__all__'
+        labels = {
+            'name': 'Наименование',
+            'category': 'Категория',
+            'price': 'Цена',
+            'amortization': 'Процент амортизации',
+            'is_monthly_amortizations': 'Ежемесячная амортизация',
+            'operation_life': 'Срок эксплуатации'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].empty_label = "Выберите категорию"
 
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+        labels = {
+            'name': 'Наименование',
+            'description': 'Описание'
+        }
 
 
 class StorageForm(forms.ModelForm):
-    amount = forms.IntegerField()
+    amount = forms.IntegerField(label="Количество")
 
     class Meta:
         model = Storage
         exclude = ('expiration_date', 'current_price')
+        labels = {
+            "item": "Предмет",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['item'].empty_label = "Выберите тип"
